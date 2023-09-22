e46bfb7c2b8450747947a9bf61d9d5516d010c87
thrikeswar <thrikeshparthiban@gmail.com>
2023-09-22 10:35:10 +0530

    inital comment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ModelComponent from './components/ModelComponent/ModelComponent'
+
+function App() {
+  return (
+    <React.Fragment>
+      <ModelComponent></ModelComponent>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/components/ModelComponent/ModelComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ModelComponent() {
+  return (
+    <React.Fragment>
+        <div>ModelComponent</div>
+    </React.Fragment>
+    
+  )
+}
+
+export default ModelComponent
